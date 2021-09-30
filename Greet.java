6e7281c4abfcac9d8bc949459b8304a834258589
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-09-30 12:12:53 -0400

    Create Greet.java

---
+++ Greet.java
@@ -0,0 +1,10 @@
+// Nafiz Labib
+// APCS
+//HW 07 - Hello Class 
+// 2021-09-30
+
+public class Greet {
+  public static void main (String [] args){
+    System.out.println("No hablo queso");
+  }
+}
