5e6fc3600571a52ce80687dafee5ff7926c6b3ca
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-03 21:14:06 -0400

    Update Greet.java

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,18 @@
+// Nafiz Labib
+// APCS
+// HW 08 - Refactor Freshie Zero
+// 2021-10-01
+
+public class Greet {
+  public static void main (String [] args){
+    System.out.println("No hablo queso");
+  }
+}
+
+//Strangely enough, running the original program without any of today's changes doesn't work.
+//I think it was because the class didn't match the file.
+//Now I get a new error saying "Editor does not contain a main type"
+//In a different file, I get an error with "Exception in thread "main" java.lang.Error: Unresolved compilation problem:" 
+//Might be due to the platform I'm trying to test this on (Eclipse).
+
+// NOT FINISHED
