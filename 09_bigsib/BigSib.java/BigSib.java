5a80fd329d3a713aa8e7d54f94db16a5d2791984
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-03 21:16:44 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java/BigSib.java
+++ 09_bigsib/BigSib.java/BigSib.java
@@ -1,3 +1,12 @@
+// Nafiz Labib and Ducky Martha, Jacob Kirmayer and Ducky Jacob Jr, Ariella Katz and Ducky Tom
+// APCS
+// HW 09 - What Are Big Sibs For?
+// 2021-10-03
+
+
+
+
+
 public class BigSib {
     public static void main( String[] args ) {
         BigSib.greet("Flim");
