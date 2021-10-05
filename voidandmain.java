1635b891b35887e25ea6f677a1ac2159dfb5c17c
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 18:10:40 -0400

    Update voidandmain.java

--- voidandmain.java
+++ voidandmain.java
@@ -5,9 +5,7 @@
       }
 
       public static void main(String[] args){
-          printHello("Bob");
-          printHello("Tom");
-          printHello("Sally");
+          printHello()
       }
 
   }
