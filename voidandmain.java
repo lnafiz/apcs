0c653fad49a00caea824a61f62c5c39d85087170
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 00:47:15 -0400

    Update voidandmain.java

--- voidandmain.java
+++ voidandmain.java
@@ -1,13 +1,16 @@
  public class voidandmain {
 
-      public static void printHello(){
-          System.out.println("Why, hello there. how do you do?");
+      public static void printHello(String){
+          System.out.println("Why, hello there, String, how do you do?");
       }
 
       public static void main(String[] args){
           String s;
+       char String = "Bob";
           printHello();
+       char String = "Tom";
           printHello();
+       char String = "Sally";
           printHello();
       }
 
