4c82fb4e8fbbb4940aa40e7d9d59abed3749d28c
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 19:16:18 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -6,6 +6,6 @@
 public class BigSib { 
       
       public static void greet(String name){
-       System.out.println("Why, hello there, " + name + ", how do you do?");
+      return ("Why, hello there, " + name + ", how do you do?");
              }
 }
