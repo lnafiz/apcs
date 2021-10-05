533f30421f4920bdefa136ac37122dff5fd13460
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 19:08:46 -0400

    Update BigSib.java

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,11 @@
+// Phase 0: Nafiz (Martha), Jacob (Jacob Jr.), Ariella (Tom)
+// APCS
+// HW 09 - What Are Big Sibs Good For?
+// 2021-10-03
+
+public class BigSib { 
+      
+      public static void greet(String name){
+       System.out.println("Why, hello there, " + name + ", how do you do?");
+             }
+}
