6063a79f147b909bf30514438bd1ff6e42951805
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 18:42:36 -0400

    Update Greet.java

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+public class Greet {
+
+      public static void greet(String name){
+       System.out.println("Why, hello there, " + name + ", how do you do?");
+      }
+
+      public static void main(String[] args){
+          greet("Foo");
+          greet("Moo");
+          greet("Boo");
+      }
+
+  }
