4c5dfd5eaac549ab96821fabb41bc7e1f6b1689f
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 19:07:55 -0400

    Update Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,14 @@
+// Phase 0: Nafiz (Martha), Jacob (Jacob Jr.), Ariella (Tom)
+// APCS
+// HW 09 - What Are Big Sibs Good For?
+// 2021-10-03
+
+
+public class Greet {
+      public static void main(String[] args){
+          BigSib.greet("Flim");
+          BigSib.greet("Flam");
+          BigSib.greet("Kazaam");
+      }
+
+  }
