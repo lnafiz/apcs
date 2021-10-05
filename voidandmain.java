78770f820eb371af566041b00639edafc8359acf
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 00:22:27 -0400

    Update voidandmain.java

---
+++ voidandmain.java
@@ -0,0 +1,12 @@
+ public class Hello {
+
+      public static void printHello(){
+          System.out.println("Hello world!");
+      }
+
+      public static void main(String[] args){
+          String s;
+          printHello();
+      }
+
+  }
