e293ffe3237bfd364a544b04c5a0b23f05cf25d3
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 21:54:05 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,3 +1,15 @@
+// Anthony Sun + Corn, Nafiz Labib + Martha
+// APCS
+// HW12 - On Elder Individuality and the Elimination of Radio Fuzz
+// 2021-10-05
+ 
+// DISCOVERIES:
+//  - Static is not needed for instance method
+//  - Generate an object with "new" keyword
+// 
+//  QCC:
+// - Is there a way to implement differences between different objects of the same class?
+
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
