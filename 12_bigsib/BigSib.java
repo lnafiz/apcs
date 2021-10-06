fd66861837cf634bae9c5b2bf6a121f99c546925
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 21:51:19 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,11 +1,11 @@
-public class BigSib { 
-      String helloMsg;
-      
-      public void sethelloMsg(String msg){
-            helloMsg = msg
-             }
-     
-      public String greet(String msg) {
+public class BigSib {
+	String helloMsg;
+
+	public void setHelloMsg(String msg) {
+		helloMsg = msg;
+	}
+
+	public String greet(String msg) {
 		return helloMsg + " " + msg;
-      }
+	}
 }
