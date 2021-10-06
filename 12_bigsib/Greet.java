20ded0ff7d2bf38faa978c91d949730c080afb10
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-05 21:49:11 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -3,9 +3,21 @@ public class Greet {
     String greeting;
 
     BigSib richard = new BigSib();
+    
     richard.setHelloMsg("Word up");
-
     greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    
+    richard.setHelloMsg("Salutations");
+    greeting = richard.greet("Dr. Spaceman");
+    System.out.println(greeting);
+    
+    richard.setHelloMsg("Hey ya");
+    greeting = richard.greet("Kong Fooey");
+    System.out.println(greeting);
+    
+    richard.setHelloMsg("Sup");
+    greeting = richard.greet("mom");
     System.out.println(greeting);
 
   } //end main()
