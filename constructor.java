c44ef2c639fed8f05a612f83a4f4ac898c347923
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-06 19:59:55 -0400

    Update constructor.java

---
+++ constructor.java
@@ -0,0 +1,13 @@
+public class BigSib {
+	String helloMsg;
+
+	public void setHelloMsg(String msg) {
+		helloMsg = msg;
+	}
+
+	public String greet(String msg) {
+		return helloMsg + " " + msg;
+	}
+}
+© 2021 GitHub, Inc.
+Terms
