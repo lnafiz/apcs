de7057d85f62e5427f1543c1772f3441ebef4a5e
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-06 21:18:27 -0400

    Update BigSib.java

--- BigSib.java
+++ BigSib.java
@@ -1,3 +1,17 @@
+// Anthony Sun + corn, Nafiz Labib + Martha
+// APCS
+// HW 13 -- constructors
+// 2021-10-06
+
+// DISCOVERIES:
+// - You can run tasks in constructor, which are run immediately after object is created with "new"
+// - Anthony combined the constuctor and setHelloMsg methods together, while I kept them separate. It works either way.
+// - No return types needed for constructor.
+
+// QCC:
+// - Is there a way to implement differences between different objects of the same class?
+
+
 public class BigSib {
 	String helloMsg;
 	
