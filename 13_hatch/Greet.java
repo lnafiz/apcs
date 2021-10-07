507eb41eca026ef6c636afaac89b4323c8cd18d5
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-06 21:21:49 -0400

    Update Greet.java

---
+++ 13_hatch/Greet.java
@@ -0,0 +1,26 @@
+// Anthony Sun + Corn, Nafiz Labib + Martha
+// APCS
+// HW 13 - Where Do BigSibs Come From?
+// 2021-10-06
+
+// DISCOVERIES:
+// - You can run tasks in constructor, which are run immediately after object is created with "new"
+// - Anthony combined the constuctor and setHelloMsg methods together, while I kept them separate. It works either way.
+// - No return types needed for constructor.
+
+// QCC:
+// - Is there a way to implement differences between different objects of the same class?
+
+
+public class Greet {
+  public static void main( String[] args ) {
+    
+    String greeting;
+    BigSib richard = new BigSib("word up");
+    
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    
+   
+  } //end main()
+} //end Greet
