10298a953b7cdfbb5c3976ea26cf85081f4a8dcd
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-06 20:54:02 -0400

    Update BigSib.java

--- BigSib.java
+++ BigSib.java
@@ -1,10 +1,9 @@
 public class BigSib {
 	String helloMsg;
-	BigSib richard = new BigSib()
+	
 		
 	public BigSib (){
-
-		richard.setHelloMsg("Word up");	
+	richard.setHelloMsg("Word up");	
 	}
 	
 	public void setHelloMsg(String msg) {
