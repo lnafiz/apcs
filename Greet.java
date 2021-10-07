88580331093329c809b44ac9477448f6cb84ab2c
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-06 20:27:40 -0400

    Update Greet.java

--- Greet.java
+++ Greet.java
@@ -1,9 +1,8 @@
 public class Greet {
   public static void main( String[] args ) {
-    String greeting;
-
-    BigSib richard = new BigSib();
     
+    String greeting;
+    BigSib richard = new BigSib();
     
     greeting = richard.greet("freshman");
     System.out.println(greeting);
