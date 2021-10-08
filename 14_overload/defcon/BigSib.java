7c4a065018638bfcd2bee5705157766260624101
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-08 00:50:21 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -1,9 +1,23 @@
+// Phase 0 Without Jacob: Nafiz, Alif, Areilla
+// APCS
+// HW 14 - Customize Your Creation
+// 2021-10-08
+
+// DISCO:
+// - With other constructors that have specified parameters, default constructor gets overridden. 
+// - Must specify default to be able to use it.
+
+// QCC:
+// - How do I specify a default???
+
+
+
+
 public class BigSib {
 	String helloMsg;
 	
 	public BigSib(){
 	
-	helloMsg = 
 	}
 	
 	public BigSib(String msg) {
