06ed90708cf12657fc4864bba58c796f1ec1611e
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-07 23:52:36 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -4,6 +4,12 @@ public class BigSib {
 	public BigSib(String msg) {
 		helloMsg = msg;
 	}
+	public BigSib(){
+	richard.setHelloMsg("Word up");
+	grizz.setHelloMsg("Hey ya");
+	dotCom.setHelloMsg("Sup");
+	tracy.setHelloMsg("Salutations");
+	}
 
 	public String greet(String msg) {
 		return helloMsg + " " + msg;
