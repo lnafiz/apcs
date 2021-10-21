25053abcc1bc64f73d8cf1efc41b25a6e68b47f5
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-20 20:56:20 -0400

    Update StatsDriver.java

--- 21_Stats/StatsDriver.java
+++ 21_Stats/StatsDriver.java
@@ -16,51 +16,32 @@ public class StatsDriver{
 	// returns 7.5
 	System.out.println(teacher.mean(-10,5));
 	// returns -2
-	teacher.mean(0);
-	// Should not run
-	teacher.mean(0,0,0);
-	// Should not run
-	teacher.mean();
-	// Should not run
-		
 /*
-	
-	teacher.mean(1,2);
-	
-	teacher.mean(10,10);
-	// Expected value should be 10
-	teacher.mean(-10,-10);
-	// Expected value should be -10
-	teacher.mean(-10.0,-2.0);
-	// Expected value should be -6.0
-	teacher.mean(0,10);
-	// Expected Value should be 5
-	teacher.mean(10,5.0);
-	// Expected Value should be 7.5
-	teacher.mean(-10,5);
-	// Expected Value should be -2.5
-	teacher.mean(0);
-	// Should not run
-	teacher.mean(0,0,0);
-	// Should not run
-	teacher.mean();
-	// Should not run
-	
+	System.out.println(teacher.mean(0));
+	// Does not run
+	System.out.println(teacher.mean(0,0,0));
+	// Does not run
+	System.out.println(teacher.mean());
+	// does not run
+	THESE DO NOT RUN BECAUSE THESE ARE NOT APPLICABLE TO MEAN METHODS
+*/		
 	teacher.max((int) 1,(int) 2);
 	// object teacher carries out method max, which compares arguments 1 and 2 and returns the greatest value [1 < 2, 2]. The returned value is an integer.
 	teacher.max((double) 1,(double) 2);
 	// object teacher carries out method max, which compares arguments 1 and 2 and returns the greatest value [1 < 2, 2]. The returned value is a double.
 	teacher.max(0,0);
 	// Should return 0
-	teacher.max();
-	// Should not run
 	teacher.max(1,2,3);
 	// Should not run
 	teacher.max(10,10.0);
 	// Should return 10.0
 	teacher.max(-10,0);
 	// Should return 0
-	
+/*
+	teacher.max();
+	// Should not run
+*/
+/*
 	teacher.geoMean((int) 1,(int) 2);
 	// object teacher carries out method geoMean, which takes the product of arguments 1 and 2 and raises it to the 1/nth power (in this case, n = 2). The returned value is an integer.
 	teacher.geoMean((double) 1,(double) 2);
