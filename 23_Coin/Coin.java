e512f09ef0318fee4facfa84bc806f6ac9f56765
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-24 22:37:59 -0400

    Update Coin.java

--- 23_Coin/Coin.java
+++ 23_Coin/Coin.java
@@ -52,5 +52,5 @@ public class Coin {
   }
   upFace = "heads";                                                                                                     
   }
-
+}
                                                                                                        
