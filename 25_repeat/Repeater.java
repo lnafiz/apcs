63fcc8737560a53d0ac54e71d1521eae18fbdab8
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-26 21:13:09 -0400

    Update Repeater.java

--- 25_repeat/Repeater.java
+++ 25_repeat/Repeater.java
@@ -1,10 +1,20 @@
 public class Repeater{
   String fenceW(int numPosts){
-  
+    int post = 1;
+    String fence = "|";
+    while(post < numPosts){
+    fence+="--|";
+    post++; // code for increment  
+  } 
   } //end of fenceW
  /* 
   String fenceR(int numPosts){
   
   } //end of fenceR
   */
+public static void main(String[] args){
+  System.out.println(fenceW(1));
+  System.out.println(fenceW(3));
+
+} //end of main
 } //end of class
