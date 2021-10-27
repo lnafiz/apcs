b7609a8bbd9bbee129e02e116c8f01d9898ecd90
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-26 21:45:45 -0400

    Update Repeater.java

--- 25_repeat/Repeater.java
+++ 25_repeat/Repeater.java
@@ -1,3 +1,16 @@
+/***
+Nafiz Labib, Jing Yi Feng - Dream Team
+APCS
+HW 25 - Do I repeat myself?
+2021-10-26
+Time Spent: ~ 20 minutes
+DISCO - 
+- Without any conditions addressing negative integer inputs, you run into errors with fenceR. You'll get a StackOverflow error because there's no limit for when post creation should be stopped (when posts reach 1).
+- Need to make another variable for the number of matches, and make sure its value increases after each match.
+
+QCC - N/A
+***/
+
 public class Repeater{
   
   public static String fenceW(int numPosts){
@@ -5,7 +18,7 @@ public class Repeater{
     int post = 1;
     String fence = "|";
     
-    while(post < numPosts && numPosts > 0){
+    while(post < numPosts){
     fence+="--|";
     post++; // code for increment  
   }
@@ -18,7 +31,7 @@ public class Repeater{
     return "|";
       }
     else{
-    return fenceR(numPosts-1)+"--|";}
+    return fenceR(numPosts-1)+"--|";} // this line creates recursion, as it returns a method with argument which must be carried out.
   } //end of fenceR
   
 public static void main(String[] args){
