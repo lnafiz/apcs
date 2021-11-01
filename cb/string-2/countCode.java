20982a43546292a65cfb783abdbdc19f9d3deaa7
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-10-31 22:31:33 -0400

    Update countCode.java

--- cb/string-2/countCode.java
+++ cb/string-2/countCode.java
@@ -1,8 +1,9 @@
-public int countHi(String str) {
-  int hi = 0;
-  for (int i=0; i<str.length()-1;i++){
-    if (str.substring(i,i+2).equals("hi"))
-    hi +=1;
+public int countCode(String str) {
+  int code = 0;
+  for (int i = 0; i < str.length()-3; i++){
+    if (str.substring(i,i+2).equals("co") && (str.substring(i+3,i+4).equals("e"))){
+      code += 1;
     }
-    return hi;
   }
+  return code;
+}
