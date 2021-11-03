9d6a6dd1c9ac6345b9c198f350a2f08685a2e81b
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-11-02 23:20:17 -0400

    Update Commafier.java

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -1,24 +1,24 @@
 public class Commafier {
-    public static String commafyR(int input){
-      String stringInput = Integer.toString(input);
-      if (stringInput.length() <= 3){ 
-        return stringInput;
+    public static String commafyR(int number){
+      String stringNumber = Integer.toString(number);
+      if (stringNumber.length() <= 3){ 
+        return stringNumber;
       }
-      else{ // recursive reduction
-        return commafyR(Integer.parseInt(stringInput.substring(0, stringInput.length() - 3))) + "," + stringInput.substring(stringInput.length() - 3, stringInput.length());
+      else{
+        return commafyR(Integer.parseInt(stringNumber.substring(0, stringNumber.length()-3))) + "," + stringNumber.substring(stringNumber.length()-3, stringNumber.length());
       }
     }
 
-    public static String commafyF(int input){
-      String stringInput = Integer.toString(input);
-      for (int counter = 3; counter < stringInput.length(); counter += 4){ // changes by 4 to account for extra comma character
-        stringInput = stringInput.substring(0, stringInput.length() - counter) + "," + stringInput.substring(stringInput.length() - counter, stringInput.length());
+    
+    public static String commafyF(int number){
+      String stringNumber = Integer.toString(number);
+      for (int a = 3; a < stringNumber.length(); a += 4){
+        stringNumber = stringNumber.substring(0, stringNumber.length()-a) + "," + stringNumber.substring(stringNumber.length()-a, stringNumber.length());
       }
       return stringInput;
     }
 
     public static void main(String[] args) {
-      // foreach loop
       for (String arg : args){
         int intArg = Integer.parseInt(arg);
         System.out.println(commafyR(intArg));
