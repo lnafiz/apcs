07d6c54c667549c04406af637920c38a7c2b5ba0
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-11-09 20:00:31 -0500

    Update Pig.java

--- lab00_oink/v2/Pig.java
+++ lab00_oink/v2/Pig.java
@@ -204,7 +204,7 @@ public class Pig {
     String ans = "";
 
 	      
-    if (VOWELSNOY.indexOf(w.substring(0,1) != -1))
+    if (VOWELSNOY.indexOf(w.substring(0,1)) != -1)
       ans = w + "way";
       
     else {
