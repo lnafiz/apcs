f8d92ce4a9928539c42ac28475c169ac06eae42b
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-11-09 21:18:04 -0500

    Update Pig.java

--- lab00_oink/Pig.java
+++ lab00_oink/Pig.java
@@ -1,3 +1,4 @@
+/***
 Heroes of the Zeroes - Nafiz Labib, Jing Yi Feng, Jomin Zhang
 APCS
 L00: Etterbay Odingcay Oughthray Ollaborationcay
@@ -9,6 +10,7 @@ DISCO -
 - The rule for y as consonant or vowel is mainly phonetic, which makes it hard to put exact rules in code. So, we did it partially, at least to satisfy the words given. Usually, if a word starts with y or a syllable in a word starts with y, the y is treated as a consonant. Usually it's a vowel when found elsewhere, whether in the middle or the very end.
 QCC - 
 - How do we have a whole sentence appear once rather than have the program run for every word in the sentence?
+
 HOW WE UTILIZED SCANNER DEMO - 
 
 WHAT CAUSES RUNTIME ERROR IN SCANTIME DEMO - 
@@ -16,4 +18,6 @@ WHAT CAUSES RUNTIME ERROR IN SCANTIME DEMO -
 NEW IN V_:
 V0 - First Pig.java file supplied in ap251/library repo, without methods and instance variables supplied in lab
 V1 - Pig.java includes methods and instance variables supplied in lab, tested and verified working. The general rules were tested and work. 
-V2 - Pig.java hass all rules tested and work EXCEPT Extra Rule #1(Consider y a vowel when appropirate).
+V2 - Pig.java has all rules tested and work EXCEPT Extra Rule #1(Consider y a vowel when appropirate).
+V3 - Pig.java has all rules tested and work (somewhat, see DISCO for further details.)
+***/
