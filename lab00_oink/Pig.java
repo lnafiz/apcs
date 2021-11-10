34fa28777c499976cbabdcbbe082695fb9fa1843
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-11-09 23:19:17 -0500

    Update Pig.java

--- lab00_oink/Pig.java
+++ lab00_oink/Pig.java
@@ -3,18 +3,21 @@ Heroes of the Zeroes - Nafiz Labib, Jing Yi Feng, Jomin Zhang
 APCS
 L00: Etterbay Odingcay Oughthray Ollaborationcay
 2021-11-09
-Time Spent: 
+Time Spent: 2.0 hours
 
 DISCO - 
 - We added an instance called CAPSVOWELS and used it in vowel-related methods, such as beginsWithVowel. 
 - The rule for y as consonant or vowel is mainly phonetic, which makes it hard to put exact rules in code. So, we did it partially, at least to satisfy the words given. Usually, if a word starts with y or a syllable in a word starts with y, the y is treated as a consonant. Usually it's a vowel when found elsewhere, whether in the middle or the very end.
+- Scanner fails at "zzz"
 QCC - 
 - How do we have a whole sentence appear once rather than have the program run for every word in the sentence?
+- How do we 
 
 HOW WE UTILIZED SCANNER DEMO - 
 - Placed method for scanner within Pig.java and then created a new scanner object in main method to scan words.in file and return strings.  
 
 WHAT CAUSES RUNTIME ERROR IN SCANTIME DEMO - 
+- hasNext() is not checked before calling next() the second time in the loop.
 
 NEW IN V_:
 V0 - First Pig.java file supplied in ap251/library repo, without methods and instance variables supplied in lab
