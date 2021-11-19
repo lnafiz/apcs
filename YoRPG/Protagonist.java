7ff37f165d53ac1a195053e1da468849283966e1
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-11-19 12:29:11 -0500

    Update Protagonist.java

--- YoRPG/Protagonist.java
+++ YoRPG/Protagonist.java
@@ -2,6 +2,7 @@ public class Protagonist{
   private String name;
   private int health = 100;
   private int nextAttackDmg;
+  private int damage;
   
   public Protagonist(String input){
   name = input;
@@ -15,9 +16,20 @@ public class Protagonist{
     return health > 0;
   }
   
-  public void changeHealth(){
+  public void changeHealth(int damage){
     health -= damage;
   }  
   
+  public void specialize(){
+    
   }
+  
+  public void normalize(){
+  
+  }
+  
+  public int attack(){
+  
+  }
+
 }// end of class
