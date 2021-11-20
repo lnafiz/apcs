59d04e8f88e2ae81d83ddb67f8eb63ce4a3ae6b4
lnafiz <75277473+lnafiz@users.noreply.github.com>
2021-11-20 18:28:06 -0500

    Update Monster.java

--- YoRPG/Monster.java
+++ YoRPG/Monster.java
@@ -1,21 +1,24 @@
-public class Monster{
-  private String name;
-  private int health = 0;
-  private nextAttackDmg;
-  
-  public Monster(String input){
-    name = input;
+public class Monster {
+  private int health = 100;
+  private int nextAttackDmg = 20;
+
+  public int attack(Protagonist hero){
+    double chance = Math.random();
+    if (chance > 0.3){
+      hero.changeHealth(nextAttackDmg);
+    }
+    else{
+      hero.changeHealth(0);
+      return 0;
+    }
+    return nextAttackDmg;
   }
-  
+
+  public void changeHealth(int damage){
+    health -= damage;
+  }
+
   public boolean isAlive(){
     return health > 0;
   }
-  
-  public int changeHealth(){
-    health -= damage;
-  }
-  
-  public int attack(){
-  
-  }
-}// end class
+}
