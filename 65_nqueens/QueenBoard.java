154a9cff595958abb67f80a6219eea2c8a25eeb6
lnafiz <nlabib20@stuy.edu>
2022-02-16 13:21:39 -0500

    thissucks

---
+++ 65_nqueens/QueenBoard.java
@@ -0,0 +1,189 @@
+/***
+ * class QueenBoard
+ * Generates solutions for N-Queens problem.
+ * USAGE:
+ * 1. Peruse. Transcribe your KtS verbiage into block comments preceding each method where necessary.
+ * 2. Implement solver method.
+ */
+
+public class QueenBoard
+{
+
+  private int[][] _board;
+
+  public QueenBoard( int size )
+  {
+    _board = new int[size][size];
+  }
+
+
+  /***
+   * precondition: board is filled with 0's only.
+   * postcondition:
+   * If a solution is found, board shows position of N queens,
+   * returns true.
+   * If no solution, board is filled with 0's,
+   * returns false.
+   */
+  public boolean solve()
+  {
+
+    return false;
+  }
+
+
+  /**
+   *Helper method for solve.
+   */
+  private boolean solveH( int col )
+  {
+    if (col = _board.length - 1){
+      for (int i = 0; i < _board.length; i++){
+        if (_board[i][col] == 0){
+          addqueen(i, col);
+        }
+        toString();
+        removeQueen(i, col);
+      }
+    }
+    else{
+      if(solveH(col - 1)== true){
+        for (int i = 0; i < _board.length; i++){
+          if (_board[i][col] == 0){
+            addqueen(i, col);
+          }
+          removeQueen(i, col);
+        }
+      }
+    }
+    return false;
+  }
+
+
+  public void printSolution()
+  {
+    /** Print board, a la toString...
+        Except:
+        all negs and 0's replaced with underscore
+        all 1's replaced with 'Q'
+    */
+  }
+
+
+
+  //================= YE OLDE SEPARATOR =================
+
+  /***
+   * <General description>
+   * precondition:
+   There are no other queens in the same row or column.
+
+   * postcondition:
+    If a square is not attacked, the queen is placed on inputted row and column.
+   */
+  private boolean addQueen(int row, int col)
+  {
+    if(_board[row][col] != 0){
+      return false;
+    }
+    _board[row][col] = 1;
+    int offset = 1;
+    while(col+offset < _board[row].length){
+      _board[row][col+offset]--;
+      if(row - offset >= 0){
+        _board[row-offset][col+offset]--;
+      }
+      if(row + offset < _board.length){
+        _board[row+offset][col+offset]--;
+      }
+      offset++;
+    }
+    return true;
+  }
+
+
+  /***
+   * <General description>
+   * precondition:
+   There is at least one other queen in the same row or column.
+   * postcondition:
+    The queen is removed from inputted row and column.
+   */
+  private boolean removeQueen(int row, int col){
+    if ( _board[row][col] != 1 ) {
+      return false;
+    }
+    _board[row][col] = 0;
+    int offset = 1;
+
+    while( col+offset < _board[row].length ) {
+      _board[row][col+offset]++;
+      if( row - offset >= 0 ) {
+        _board[row-offset][col+offset]++;
+      }
+      if( row + offset < _board.length ) {
+        _board[row+offset][col+offset]++;
+      }
+      offset++;
+    }
+    return true;
+  }
+
+
+  /***
+   * <General description>
+   * precondition:
+   an int[][] exists
+   * postcondition:
+   converts all values i every index and array to String output.
+   */
+  public String  toString()
+  {
+    String ans = "";
+    for( int r = 0; r < _board.length; r++ ) {
+      for( int c = 0; c < _board[0].length; c++ ) {
+        ans += _board[r][c]+"\t";
+      }
+      ans += "\n";
+    }
+    return ans;
+  }
+
+
+  //main method for testing...
+  public static void main( String[] args )
+  {
+    QueenBoard b = new QueenBoard(5);
+    System.out.println(b);
+    /** should be...
+       0	0	0	0	0
+       0	0	0	0	0
+       0	0	0	0	0
+       0	0	0	0	0
+       0	0	0	0	0
+    */
+
+    b.addQueen(3,0);
+    b.addQueen(0,1);
+    System.out.println(b);
+    /** should be...
+       0	1	-1	-2	-1
+       0	0	-2	0	0
+       0	-1	0	-1	0
+       1	-1	-1	-1	-2
+       0	-1	0	0	0
+    */
+
+    b.removeQueen(3,0);
+    System.out.println(b);
+    /** should be...
+       0	1	-1	-1	-1
+       0	0	-1	0	0
+       0	0	0	-1	0
+       0	0	0	0	-1
+       0	0	0	0	0
+    */
+
+  }
+
+}//end class
