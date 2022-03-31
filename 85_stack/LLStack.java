c956b057379ade716b57ca91b0e77efeb4afbbe8
unknown <nlabib20@stuy.edu>
2022-03-30 20:01:56 -0400

    fixed org and finished work

---
+++ 85_stack/LLStack.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+
+public class LLStack<PANCAKE> implements Stack<PANCAKE> {
+
+  private LinkedList<PANCAKE> _stack;
+
+  public LLStack() {
+      _stack = new LinkedList<PANCAKE>();
+    }
+
+    //Return true if this stack is empty, otherwise false.
+  public boolean isEmpty(){
+        return _stack.size() == 0;
+    }
+
+    //Return top element of stack without popping it.
+  public PANCAKE peekTop(){
+        return _stack.getLast();
+    }
+
+  //Pop and return top element of stack.
+  public PANCAKE pop(){
+    if (isEmpty()){
+      return null;
+    }
+    return _stack.removeLast();
+  }
+
+  //Push an element onto top of this stack.
+  public void push( PANCAKE x ){
+    _stack.addLast(x);
+  }
+
+}
