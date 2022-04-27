e7b94521f42052c6f0a4624fc0a6e125221734b8
lnafiz <75277473+lnafiz@users.noreply.github.com>
2022-04-27 14:12:42 -0400

    Update CelebrityGame.java

--- cobo/101_celeb/CelebrityGame.java
+++ cobo/101_celeb/CelebrityGame.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 /**
  * The framework for the Celebrity Game project
- * 
+ *
  * @author cody.henrichsen
  * @version 2.3 25/09/2018 refactored the prepareGame and play methods
  */
@@ -11,20 +11,24 @@ public class CelebrityGame
 	/**
 	 * A reference to a Celebrity or subclass instance.
 	 */
+	 private Celebrity gameCelebrity;
 
 	/**
 	 * The GUI frame for the Celebrity game.
 	 */
+	 private CelebrityFrame gameWindow;
 
 	/**
 	 * The ArrayList of Celebrity values that make up the game
 	 */
-
+	 private ArrayList<Celebrity> celebGameList;
 	/**
 	 * Builds the game and starts the GUI
 	 */
 	public CelebrityGame()
 	{
+		celebGameList = new ArrayList<Celebrity>();
+		gameWindow = new CelebrityFrame(this);
 	}
 
 	/**
@@ -32,11 +36,13 @@ public class CelebrityGame
 	 */
 	public void prepareGame()
 	{
+		celebGameList = new ArrayList<Celebrity>();
+		gameWindow.replaceScreen("START");
 	}
 
 	/**
 	 * Determines if the supplied guess is correct.
-	 * 
+	 *
 	 * @param guess
 	 *            The supplied String
 	 * @return Whether it matches regardless of case or extraneous external
@@ -44,7 +50,20 @@ public class CelebrityGame
 	 */
 	public boolean processGuess(String guess)
 	{
-		return false;
+		boolean result = false;
+		guess = guess.trim();
+		if (guess.equalsIgnoreCase( gameCelebrity.getAnswer() )){
+			celebGameList.remove(0);
+			result = true;
+
+			if ( getCelebrityGameSize() > 0){
+				gameCelebrity = celebGameList.get(0);
+			}
+			else{
+				gameCelebrity = new Celebrity("", "");
+			}
+		}
+		return result;
 	}
 
 	/**
@@ -54,12 +73,16 @@ public class CelebrityGame
 	 */
 	public void play()
 	{
-		
+		if (celebGameList != null && celebGameList.size() > 0){
+			gameCelebrity = celebGameList.get(0);
+			gameWindow.replaceScreen("GAME");
+		}
+
 	}
 
 	/**
 	 * Adds a Celebrity of specified type to the game list
-	 * 
+	 *
 	 * @param name
 	 *            The name of the celebrity
 	 * @param guess
@@ -69,7 +92,8 @@ public class CelebrityGame
 	 */
 	public void addCelebrity(String name, String guess, String type)
 	{
-		
+		Celebrity newCelebrity = new Celebrity(name, guess);
+		celebGameList.add(newCelebrity);
 	}
 
 	/**
@@ -79,50 +103,66 @@ public class CelebrityGame
 	 */
 	public boolean validateCelebrity(String name)
 	{
-		return false;
+		name = name.trim();
+		return name.length() >= 4;
 	}
 
 	/**
 	 * Checks that the supplied clue has at least 10 characters or is a series of clues
 	 * This method would be expanded based on your subclass of Celebrity.
 	 * @param clue The text of the clue(s)
-	 * @param type Supports a subclass of Celebrity 
+	 * @param type Supports a subclass of Celebrity
 	 * @return If the clue is valid.
 	 */
 	public boolean validateClue(String clue, String type)
 	{
+		boolean result = false;
+		clue = clue.trim();
+		if ( clue.length() >= 10){ return true; }
+		else {
+			if (type.equalsIgnoreCase("literature")){
+				String[] tmp = clue.split(",");
+				if (tmp.length > 1){ return true; }
+			}
+			if (type.equalsIgnoreCase("sports")){
+				String[] tmp = clue.split(",");
+				if (tmp.length > 1){ return true; }
+			}
+		}
 		return false;
 	}
 
 	/**
 	 * Accessor method for the current size of the list of celebrities
-	 * 
+	 *
 	 * @return Remaining number of celebrities
 	 */
 	public int getCelebrityGameSize()
 	{
-		return 0;
+		return celebGameList.size();
 	}
 
 	/**
 	 * Accessor method for the games clue to maintain low coupling between
 	 * classes
-	 * 
+	 *
 	 * @return The String clue from the current celebrity.
 	 */
 	public String sendClue()
 	{
-		return null;
+		if (celebGameList.size() == 0){ return ""; }
+		return gameCelebrity.getClue();
 	}
 
 	/**
 	 * Accessor method for the games answer to maintain low coupling between
 	 * classes
-	 * 
+	 *
 	 * @return The String answer from the current celebrity.
 	 */
 	public String sendAnswer()
 	{
-		return null;
+		if (celebGameList.size() == 0){ return ""; }
+		return gameCelebrity.getAnswer();
 	}
 }
