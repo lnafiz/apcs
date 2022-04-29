ccd7eabe3b11d87cb21c61f9f836bda7e54cf443
unknown <nlabib20@stuy.edu>
2022-04-28 21:48:36 -0400

    tested

---
+++ srijal30celeb/101_celeb/CelebrityGame.java
@@ -0,0 +1,198 @@
+/*
+TNPG: Flying Sullen Actors (Alif Rahman, Salaj Rijal, Faiyaz Rafee)
+APCS
+L09: Some Folks Call It A Charades
+2022-04-27
+time spent: 4 hours
+*/
+
+import java.util.ArrayList;
+
+/**
+ * The framework for the Celebrity Game project
+ *
+ * @author cody.henrichsen
+ * @version 2.3 25/09/2018 refactored the prepareGame and play methods
+ */
+public class CelebrityGame
+{
+	/**
+	 * A reference to a Celebrity or subclass instance.
+	 */ //<---- makes no sense
+	 private Celebrity gameCelebrity;
+
+	/**
+	 * The GUI frame for the Celebrity game.
+	 */
+	private CelebrityFrame gameWindow;
+
+
+	/**
+	 * The ArrayList of Celebrity values that make up the game
+	 */
+	private ArrayList<Celebrity> celebGameList;
+
+	/**
+	 * Builds the game and starts the GUI
+	 */
+	public CelebrityGame()
+	{
+		//prepare the game (adding celebs to the list)
+		prepareGame();
+	}
+
+	/**
+	 * Prepares the game to start by re-initializing the celebGameList and having the gameFrame open the start screen.
+	 */
+	public void prepareGame()
+	{
+		//reinitialize (start a new game)
+		celebGameList = new ArrayList<Celebrity>();
+		gameWindow = new CelebrityFrame(this);
+
+		//open GUI frame that allows user to input celebs
+		gameWindow.replaceScreen("START");
+	}
+
+	/**
+	 * Determines if the supplied guess is correct.
+	 *
+	 * @param guess
+	 *            The supplied String
+	 * @return Whether it matches regardless of case or extraneous external
+	 *         spaces.
+	 */
+	public boolean processGuess(String guess)
+	{
+		if (celebGameList.size() <= 0) System.out.println("SOrry");
+		boolean matchFound = false;
+		guess = guess.trim();
+
+		matchFound = guess.equalsIgnoreCase( gameCelebrity.getAnswer() );
+		if( matchFound ){
+			celebGameList.remove(0);
+			gameCelebrity = celebGameList.get(0);
+		}
+
+		return matchFound;
+	}
+
+	/**
+	 * Asserts that the list is initialized and contains at least one Celebrity.
+	 * Sets the current celebrity as the first item in the list. Opens the game
+	 * play screen.
+	 */
+	public void play()
+	{
+
+		//check that celeb game list is not empty so game can actually start
+		if (celebGameList != null && celebGameList.size() > 0  )
+		{
+			this.gameCelebrity = celebGameList.get(0);
+			gameWindow.replaceScreen("GAME");
+		}
+		//(if it is empty i guess than print some message) <-- shouldnt happen tho
+		else{
+			System.out.println("CELEB GAME LIST IS EMPTY");
+		}
+	}
+
+	/**
+	 * Adds a Celebrity of specified type to the game list
+	 *
+	 * @param name
+	 *            The name of the celebrity
+	 * @param guess
+	 *            The clue(s) for the celebrity
+	 *
+	 * NOT IMPLEMENTED FOR NOW:
+	 * @param type
+	 *            What type of celebrity
+	 */
+	public void addCelebrity(String name, String guess, String type)
+	{
+		Celebrity newCeleb;
+
+		//checks what type of celeb needs to be added
+		if( type.equals("Literature") ){
+			newCeleb = new LiteratureCelebrity(name, guess);
+		}
+		else{
+			newCeleb = new Celebrity(name, guess);
+		}
+
+		//actually adds the celeb
+		celebGameList.add( newCeleb );
+	}
+
+	/**
+	 * Validates the name of the celebrity. It must have at least 4 characters.
+	 * @param name The name of the Celebrity
+	 * @return If the supplied Celebrity is valid
+	 */
+	public boolean validateCelebrity(String name)
+	{
+		name = name.trim();
+		return name.length() >= 4;
+	}
+
+	/**
+	 * Checks that the supplied clue has at least 10 characters or is a series of clues
+	 * This method would be expanded based on your subclass of Celebrity.
+	 * @param clue The text of the clue(s)
+	 * @param type Supports a subclass of Celebrity
+	 * @return If the clue is valid.
+	 */
+	public boolean validateClue(String clue, String type)
+	{
+		boolean validClue = false;
+		clue = clue.trim();
+
+		//check if at least 10 chars
+		if( clue.length() >= 10 ){
+			validClue = true;
+
+			//extra recs if
+			if ( type.equalsIgnoreCase("Literature") ){
+
+				String[] temp = clue.split(",");
+				if( temp.length > 1 ) {
+					validClue = true;
+				}
+			}
+		}
+		return validClue;
+	}
+
+	/**
+	 * Accessor method for the current size of the list of celebrities
+	 *
+	 * @return Remaining number of celebrities
+	 */
+	public int getCelebrityGameSize()
+	{
+		return celebGameList.size();
+	}
+
+	/**
+	 * Accessor method for the games clue to maintain low coupling between
+	 * classes
+	 *
+	 * @return The String clue from the current celebrity.
+	 */
+	public String sendClue()
+	{
+		return gameCelebrity.getClue();
+	}
+
+	/**
+	 * Accessor method for the games answer to maintain low coupling between
+	 * classes
+	 *
+	 * @return The String answer from the current celebrity.
+	 */
+	public String sendAnswer()
+	{
+		return gameCelebrity.getAnswer();
+	}
+}
---
+++ srijal30celeb/101_celeb/CelebrityRunner.java
@@ -0,0 +1,130 @@
+/*
+TNPG: Flying Sullen Actors (Alif Rahman, Salaj Rijal, Faiyaz Rafee)
+APCS
+L09: Some Folks Call It A Charades
+2022-04-27
+time spent: 4 hours
+*/
+
+/**
+ * Starts the Celebrity Game application
+ * @author cody.henrichsen
+ *
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CelebrityRunner
+{
+	/**
+	 * The entry point of the Java application.
+	 * @param args Unused parameter as it is a GUI based program.
+	 */
+
+
+	public static void runTerminalgame(CelebrityGame game) {
+
+		Scanner sc = new Scanner( System.in );
+
+		//add at least 1 celeb
+		System.out.print("Please Input a Celeb Name: ");
+
+		//get a valid name
+		String name = sc.nextLine();
+		while( ! game.validateCelebrity(name) ){
+			System.out.print("Please Try Again: ");
+			name = sc.nextLine();
+		}
+
+		//get a valid clue
+		System.out.print("\nPlease Input a Clue: ");
+		String clue = sc.nextLine();
+		while( ! game.validateClue(clue, "") ){
+			System.out.print("Please Try Again: ");
+			clue = sc.nextLine();
+		}
+
+		//add the first celeb
+		game.addCelebrity(name, clue, "");
+
+		//ask if they want to input more
+		System.out.print( "\nDo you want to add more Celebs?(true/false): ");
+		boolean more = sc.nextBoolean();
+
+		//add more celebs
+		while(more){
+			//get a valid name
+			System.out.print("\nPlease Input a Name: ");
+			sc.nextLine(); //idk why i need this but seems to work better
+			name = sc.nextLine();
+			while( ! game.validateCelebrity(name) ){
+				System.out.print("Please Try Again: ");
+				name = sc.nextLine();
+			}
+
+			//get a valid clue
+			System.out.print("\nPlease Input a Clue: ");
+			clue = sc.nextLine();
+			while( ! game.validateClue(clue, "") ){
+				System.out.print("Please Try Again: ");
+				clue = sc.nextLine();
+			}
+
+			//add the celeb
+			game.addCelebrity(name, clue, "");
+
+			//ask if they want more
+			System.out.print( "\nDo you want to add more Celebs?(true/false): ");
+			more = sc.nextBoolean();
+
+		}
+
+		int points = 0;
+		int currentGuesses = 5;
+
+		//play the game
+		game.play();
+
+		System.out.println("\n\nGAME STARTING...");
+		while( game.getCelebrityGameSize() > 0 ){
+			//reset guess count
+			currentGuesses = 5;
+			//print out current clue
+			System.out.println("\nYour clue: " + game.sendClue() );
+			//store the answer
+			String celeb = game.sendAnswer();
+			//keep on going while guess count higher than 0
+			while( currentGuesses > 0 ){
+				System.out.print("You have " + currentGuesses + " guesses left: ");
+				String guess = sc.nextLine();
+				//if right
+				if( game.processGuess(guess) ){
+					System.out.println("You got it right! You have " + ++points + " points!");
+					break;
+				}
+				else{
+					System.out.print("Incorrect! ");
+					currentGuesses--;
+				}
+			}
+
+			System.out.println("The answers was: " + celeb );
+		}
+
+		System.out.println("Game is over... you ended with " + points + " points!");
+		sc.close();
+	}
+
+
+	public static void main(String [] args) throws IOException
+	{
+
+		CelebrityGame game = new CelebrityGame();
+
+		/*terminal running.... make sure Gui stuff commented out*/
+		//runTerminalgame(game);
+
+
+	}
+}
